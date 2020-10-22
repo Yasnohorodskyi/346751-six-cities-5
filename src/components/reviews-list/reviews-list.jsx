a860c266d7e5385cd26fbabaf9c0347d452864cd
Andrei Korolev <andreikoroleffs@gmail.com>
2020-10-22 13:23:19 +0300

    Созданы и подключены компоненты ReviewsList, ReviewItem

---
+++ src/components/reviews-list/reviews-list.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReviewItem from "../review-item/review-item";
+
+const ReviewsList = (props) => {
+  const {reviews} = props;
+
+  return (
+    <section className="property__reviews reviews">
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
+      <ul className="reviews__list">
+        {
+          reviews.map((review, i) => (
+            <ReviewItem
+              key={`${i}-review`}
+              review={review}
+            />
+          ))
+        }
+      </ul>
+    </section>
+  );
+};
+
+ReviewsList.propTypes = {
+  reviews: PropTypes.array.isRequired,
+};
+
+export default ReviewsList;
