b791d712b55a8f82f1278c129b60c064e34d475c
Andrei Korolev <andreikoroleffs@gmail.com>
2020-10-26 20:31:44 +0300

    Удалили передачу props через App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import MainPage from "../main-page/main-page";
 import AuthScreen from "../auth-screen/auth-screen";
@@ -7,39 +6,26 @@ import FavoritesScreen from "../favorites-screen/favorites-screen";
 import PropertyScreen from "../property-screen/property-screen";
 
 
-const App = (props) => {
-  const {offers, reviews} = props;
+const App = () => {
 
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <MainPage
-            offers={offers}
-          />
+          <MainPage/>
         </Route>
         <Route exact path="/login">
           <AuthScreen/>
         </Route>
         <Route exact path="/favorites">
-          <FavoritesScreen
-            offers={offers}
-          />
+          <FavoritesScreen/>
         </Route>
         <Route exact path="/offer/:id">
-          <PropertyScreen
-            offers={offers}
-            reviews={reviews}
-          />
+          <PropertyScreen/>
         </Route>
       </Switch>
     </BrowserRouter>
   );
-};
-
-App.propTypes = {
-  offers: PropTypes.array.isRequired,
-  reviews: PropTypes.array.isRequired,
 };
 
 export default App;
