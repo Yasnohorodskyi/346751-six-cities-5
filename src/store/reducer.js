cd06868b5902cb6b95bf72147d4722133b2240cb
Andrei Korolev <andreikoroleffs@gmail.com>
2020-10-26 18:44:16 +0300

    Определенно начальное состояние хранилища

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,13 @@
 import {extend} from "../utils.js";
 import offers from "../mocs/offers";
 import reviews from "../mocs/offers";
+import {Cities} from "../const";
 
-const reducer = (state, action) => {
+const initialState = {
+  city: Cities[0],
+};
+
+export const reducer = (state = initialState, action) => {
 
   return state;
 };
-
-export {reducer};
