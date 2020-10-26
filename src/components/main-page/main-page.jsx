28abd6296cd248e434fe266936a3ea2d99b3d0f1
Andrei Korolev <andreikoroleffs@gmail.com>
2020-10-26 18:29:44 +0300

    Закомментировал фильтр на главной странице

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -63,12 +63,12 @@ const MainPage = (props) => {
                     <use xlinkHref="#icon-arrow-select"></use>
                   </svg>
                 </span>
-                <ul className="places__options places__options--custom places__options--opened">
+                {/*<ul className="places__options places__options--custom places__options--opened">
                   <li className="places__option places__option--active" tabIndex="0">Popular</li>
                   <li className="places__option" tabIndex="0">Price: low to high</li>
                   <li className="places__option" tabIndex="0">Price: high to low</li>
                   <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul>
+                </ul>*/}
               </form>
               <OffersList
                 offers={offers}
