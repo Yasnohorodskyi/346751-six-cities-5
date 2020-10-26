153cc1932e5aca53e7087e10e2b4a866c8c56de4
Andrei Korolev <andreikoroleffs@gmail.com>
2020-10-26 18:08:42 +0300

    Созданы файлы action.js, reducer.js, utils.js

---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import {extend} from "../utils.js";
+import offers from "../mocs/offers";
+import reviews from "../mocs/offers";
+
+const reducer = (state, action) => {
+
+  return state;
+};
+
+export {reducer};
