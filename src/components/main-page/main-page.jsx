18407381ce6cadf05479e627595101fa6b8dbde3
Andrei Korolev <andreikoroleffs@gmail.com>
2020-10-28 20:21:32 +0300

    Создан и подключен компонент SortingOptions.

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -8,6 +8,7 @@ import Main from "../main/main";
 import CitiesList from "../cities-list/cities-list";
 import OffersList from "../offers-list/offers-list";
 import OffersMap from "../offers-map/offers-map";
+import SortingOptions from "../sorting-options/sorting-options";
 
 
 const MainPage = ({offers, city, onCityChange}) => {
@@ -26,21 +27,7 @@ const MainPage = ({offers, city, onCityChange}) => {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{(offers.length)} places to stay in {city}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                {/* <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                  <li className="places__option" tabIndex="0">Price: low to high</li>
-                  <li className="places__option" tabIndex="0">Price: high to low</li>
-                  <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul> */}
-              </form>
+              <SortingOptions />
               <OffersList
                 offers={offers}
                 renderClassName={() => (`cities__places-list tabs__content`)}
