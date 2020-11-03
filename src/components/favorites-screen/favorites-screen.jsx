156d01075ff00f94bd0583f5a11d968bb82c256d
Andrei Korolev <andreikoroleffs@gmail.com>
2020-11-03 19:54:29 +0300

    Рефакторинг FavoritesScreen с помощью hoc withActiveCard

--- src/components/favorites-screen/favorites-screen.jsx
+++ src/components/favorites-screen/favorites-screen.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 
@@ -7,102 +7,84 @@ import Main from "../main/main";
 import FavoriteCard from "../favorite-card/favorite-card";
 import Footer from "../footer/footer";
 
-class FavoritesScreen extends PureComponent {
-  constructor(props) {
-    super(props);
+const FavoritesScreen = ({offers, onCardMouseOver}) => {
 
-    this.state = {
-      activeCard: ``,
-    };
-  }
+  return (
+    <div className="page">
+      <Header/>
+      <Main renderClassName={() => (`page__main--favorites`)}>
+        <div className="page__favorites-container container">
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <ul className="favorites__list">
+              {
 
-  render() {
-    const {offers} = this.props;
-
-    return (
-      <div className="page">
-        <Header/>
-        <Main renderClassName={() => (`page__main--favorites`)}>
-          <div className="page__favorites-container container">
-            <section className="favorites">
-              <h1 className="favorites__title">Saved listing</h1>
-              <ul className="favorites__list">
-                {
-
-                }
-                <li className="favorites__locations-items">
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <a className="locations__item-link" href="#">
-                        <span>Amsterdam</span>
-                      </a>
-                    </div>
+              }
+              <li className="favorites__locations-items">
+                <div className="favorites__locations locations locations--current">
+                  <div className="locations__item">
+                    <a className="locations__item-link" href="#">
+                      <span>Amsterdam</span>
+                    </a>
                   </div>
-                  <div className="favorites__places">
-                    {
-                      offers[`Amsterdam`].map((offer) => {
-                        if (offer.bookmark) {
-                          return (
-                            <FavoriteCard
-                              key={`${offer[`id`]}-${offer[`city`]}`}
-                              offer={offer}
-                              onHover={() => {
-                                this.setState(() => ({
-                                  activeCard: offer[`id`],
-                                }));
-                              }}
-                            />
-                          );
-                        }
+                </div>
+                <div className="favorites__places">
+                  {
+                    offers[`Amsterdam`].map((offer) => {
+                      if (offer.bookmark) {
+                        return (
+                          <FavoriteCard
+                            key={`${offer[`id`]}-${offer[`city`]}`}
+                            offer={offer}
+                            onHover={() => (onCardMouseOver(offer.id))}
+                          />
+                        );
+                      }
 
-                        return false;
-                      })
-                    }
-                  </div>
-                </li>
+                      return false;
+                    })
+                  }
+                </div>
+              </li>
 
-                <li className="favorites__locations-items">
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <a className="locations__item-link" href="#">
-                        <span>Cologne</span>
-                      </a>
-                    </div>
+              <li className="favorites__locations-items">
+                <div className="favorites__locations locations locations--current">
+                  <div className="locations__item">
+                    <a className="locations__item-link" href="#">
+                      <span>Cologne</span>
+                    </a>
                   </div>
-                  <div className="favorites__places">
-                    {
-                      offers[`Cologne`].map((offer) => {
-                        if (offer.bookmark) {
-                          return (
-                            <FavoriteCard
-                              key={`${offer[`id`]}-${offer[`city`]}`}
-                              offer={offer}
-                              onHover={() => {
-                                this.setState(() => ({
-                                  activeCard: offer[`id`],
-                                }));
-                              }}
-                            />
-                          );
-                        }
+                </div>
+                <div className="favorites__places">
+                  {
+                    offers[`Cologne`].map((offer) => {
+                      if (offer.bookmark) {
+                        return (
+                          <FavoriteCard
+                            key={`${offer[`id`]}-${offer[`city`]}`}
+                            offer={offer}
+                            onHover={() => (onCardMouseOver(offer.id))}
+                          />
+                        );
+                      }
 
-                        return false;
-                      })
-                    }
-                  </div>
-                </li>
-              </ul>
-            </section>
-          </div>
-        </Main>
-        <Footer/>
-      </div>
-    );
-  }
-}
+                      return false;
+                    })
+                  }
+                </div>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </Main>
+      <Footer/>
+    </div>
+  );
+};
 
 FavoritesScreen.propTypes = {
   offers: PropTypes.objectOf(PropTypes.array).isRequired,
+  onCardMouseOver: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
